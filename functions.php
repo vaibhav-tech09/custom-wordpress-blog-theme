d0b8e5befca9f94bc6b5960945a9117abe6add19
vaibhav-tech09 <09singh2019vs@gmail.com>
2026-01-17 21:21:40 +0530

    Add comments for theme setup

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
+// Theme setup and feature support functions
+
 function vaibhav_theme_setup() {
   add_theme_support('title-tag');
 }
 add_action('after_setup_theme', 'vaibhav_theme_setup');
+
